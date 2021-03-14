7d64ba22d3abd8ef6a58143db78f4b337321ac17
Carlos Rincón <carlosjrinconavila@gmail.com>
2021-03-13 22:09:48 -0500

    Added register feature

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,23 +1,5 @@
-/*!
+import React, {useEffect, useState} from "react";
 
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
-
-// reactstrap components
 import {
   Button,
   Card,
@@ -31,133 +13,201 @@ import {
   InputGroup,
   Container,
   Row,
-  Col
+  Col, Badge
 } from "reactstrap";
 
-// core components
 import Navbar from "components/Navbars/NavBar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
+import {renderFormInput} from "../../util/input.util";
+import UserService from "../../services/user.service";
+import {MASKS} from "../../util/mask.util";
+import RegistrationTypeService from "../../services/registration_type.service";
+import CurrencyFormat from 'react-currency-format';
 
-class Register extends React.Component {
-  componentDidMount() {
+const Register = () => {
+
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    addess: '',
+    phone_number: '',
+    birth_data: '',
+    password: '',
+    repeat_password: '',
+    document_numner: '',
+    country: '',
+    birth_date: ''
+  })
+  const [registrationTypes, setRegistrationTypes] = useState([])
+
+  useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
-    this.refs.main.scrollTop = 0;
+    loadRegistrationTypes();
+  }, []);
+
+  const loadRegistrationTypes = () => {
+    const registrationTypeService = new RegistrationTypeService();
+    registrationTypeService.getRegistrationTypes().then(
+      response => setRegistrationTypes(response.data)
+    )
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
   }
-  render() {
-    return (
-      <>
-        <Navbar />
-        <main ref="main">
-          <section className="section section-shaped section-lg">
-            <div className="shape shape-style-1 bg-gradient-default">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
+
+  const submitRegisterForm = (event) => {
+    event.preventDefault();
+    const userService = new UserService();
+    const createUserResponse = userService.createUser(formData)
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  const renderInput = (name, label, options = {}) => {
+    return renderFormInput(name, label, handleInputChange, options);
+  }
+
+  const renderRegistrationTypeDescriptionList = (description) => {
+    return description.split('+').map(item => (
+      <li className="py-2">
+        <div className="d-flex align-items-center">
+          <div>
+            <Badge
+              className="badge-circle mr-3"
+              color="success"
+            >
+              <i className="ni ni-check-bold"/>
+            </Badge>
+          </div>
+          <div>
+            <span className="mb-0">
+              {item}
+            </span>
+          </div>
+        </div>
+      </li>
+    ))
+  }
+
+  const renderRegistrationTypeCards = () => {
+    return registrationTypes.map(registrationType => (
+      <Col lg="4" key={registrationType.id}>
+        <Card className="card-lift--hover shadow border-0">
+          <CardBody className="py-5">
+            <div className="text-center">
+              <span className="text-primary text-uppercase" style={{fontSize: '32px', fontWeight: 700}}>
+                <CurrencyFormat value={registrationType.amount} displayType={'text'} thousandSeparator={true} prefix={'$'} /> (COP)
+              </span>
             </div>
-            <Container className="pt-lg-7">
-              <Row className="justify-content-center">
-                <Col lg="5">
-                  <Card className="bg-secondary shadow border-0">
-                    <CardHeader className="bg-white pb-5">
-                      <div className="text-muted text-center mb-3">
-                        <small>Sign up with</small>
-                      </div>
-                      <div className="text-center">
-                        <Button
-                          className="btn-neutral btn-icon mr-4"
-                          color="default"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
+            <ul className="list-unstyled mt-3">
+              {renderRegistrationTypeDescriptionList(registrationType.description)}
+            </ul>
+            <p className="description text-center bold">
+              {registrationType.limits}
+            </p>
+          </CardBody>
+        </Card>
+      </Col>
+    ));
+  }
+
+  return (
+    <>
+      <Navbar/>
+      <main>
+        <section className="section section-shaped section-lg">
+          <div className="shape shape-style-1 bg-gradient-default">
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+            <span/>
+          </div>
+          <Container className="pt-lg-7">
+            <Row className="justify-content-center">
+              <Col lg="12">
+                <Card className="bg-secondary shadow border-0">
+                  <CardHeader className="bg-white pb-5">
+                    <div className="text-muted text-center mb-3">
+                      <small>Sign up with</small>
+                    </div>
+                    <div className="text-center">
+                      <Button
+                        className="btn-neutral btn-icon mr-4"
+                        color="default"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
                           <span className="btn-inner--icon mr-1">
                             <img
                               alt="..."
                               src={require("assets/img/icons/common/github.svg")}
                             />
                           </span>
-                          <span className="btn-inner--text">Github</span>
-                        </Button>
-                        <Button
-                          className="btn-neutral btn-icon ml-1"
-                          color="default"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
+                        <span className="btn-inner--text">Github</span>
+                      </Button>
+                      <Button
+                        className="btn-neutral btn-icon ml-1"
+                        color="default"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
                           <span className="btn-inner--icon mr-1">
                             <img
                               alt="..."
                               src={require("assets/img/icons/common/google.svg")}
                             />
                           </span>
-                          <span className="btn-inner--text">Google</span>
-                        </Button>
-                      </div>
-                    </CardHeader>
-                    <CardBody className="px-lg-5 py-lg-5">
-                      <div className="text-center text-muted mb-4">
-                        <small>Or sign up with credentials</small>
-                      </div>
-                      <Form role="form">
-                        <FormGroup>
-                          <InputGroup className="input-group-alternative mb-3">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="ni ni-hat-3" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input placeholder="Name" type="text" />
-                          </InputGroup>
-                        </FormGroup>
-                        <FormGroup>
-                          <InputGroup className="input-group-alternative mb-3">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="ni ni-email-83" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input placeholder="Email" type="email" />
-                          </InputGroup>
-                        </FormGroup>
-                        <FormGroup>
-                          <InputGroup className="input-group-alternative">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="ni ni-lock-circle-open" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Password"
-                              type="password"
-                              autoComplete="off"
+                        <span className="btn-inner--text">Google</span>
+                      </Button>
+                    </div>
+                  </CardHeader>
+                  <CardBody className="px-lg-5 py-lg-5">
+                    <div className="text-center text-muted mb-4">
+                      <small>Ingresa tus datos</small>
+                    </div>
+                    <Form role="form">
+                      <Row>
+                        {renderInput("first_name", "Nombres")}
+                        {renderInput("last_name", "Apellidos")}
+                        {renderInput("country", "Pais")}
+                        {renderInput("city", "Ciudad")}
+                        {renderInput("document_number", "Número de Documento", {pattern: '[0-9]*'})}
+                        {renderInput("birth_date", "Fecha de Nacimiento", {mask: MASKS.date})}
+                        {renderInput("phone_number", "Número Telefoníco", {mask: MASKS.phone})}
+                        {renderInput("address", "Dirección", {size: 'full'})}
+                        {renderInput("email", "Email", {size: 'full'})}
+                        {renderInput("password", "Contraseña", {type: 'password'})}
+                        {renderInput("repeat_password", "Repite tu contraseña", {type: 'password'})}
+                      </Row>
+
+                      <Row>
+                        <Col md={12}>
+
+                        </Col>
+                        {renderRegistrationTypeCards()}
+                      </Row>
+
+                      <Row className="my-4">
+                        <Col xs="12">
+                          <div className="custom-control custom-control-alternative custom-checkbox">
+                            <input
+                              className="custom-control-input"
+                              id="customCheckRegister"
+                              type="checkbox"
                             />
-                          </InputGroup>
-                        </FormGroup>
-                        <div className="text-muted font-italic">
-                          <small>
-                            password strength:{" "}
-                            <span className="text-success font-weight-700">
-                              strong
-                            </span>
-                          </small>
-                        </div>
-                        <Row className="my-4">
-                          <Col xs="12">
-                            <div className="custom-control custom-control-alternative custom-checkbox">
-                              <input
-                                className="custom-control-input"
-                                id="customCheckRegister"
-                                type="checkbox"
-                              />
-                              <label
-                                className="custom-control-label"
-                                htmlFor="customCheckRegister"
-                              >
+                            <label
+                              className="custom-control-label"
+                              htmlFor="customCheckRegister"
+                            >
                                 <span>
                                   I agree with the{" "}
                                   <a
@@ -167,31 +217,32 @@ class Register extends React.Component {
                                     Privacy Policy
                                   </a>
                                 </span>
-                              </label>
-                            </div>
-                          </Col>
-                        </Row>
-                        <div className="text-center">
-                          <Button
-                            className="mt-4"
-                            color="primary"
-                            type="button"
-                          >
-                            Create account
-                          </Button>
-                        </div>
-                      </Form>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>
-          </section>
-        </main>
-        <SimpleFooter />
-      </>
-    );
-  }
+                            </label>
+                          </div>
+                        </Col>
+                      </Row>
+                      <div className="text-center">
+                        <Button
+                          className="mt-4"
+                          color="primary"
+                          type="button"
+                          onClick={submitRegisterForm}
+                        >
+                          Create account
+                        </Button>
+                      </div>
+                    </Form>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </main>
+      <SimpleFooter/>
+    </>
+  );
+
 }
 
 export default Register;
--- src/util/input.util.js
+++ src/util/input.util.js
@@ -1,33 +1,41 @@
+import React from "react";
 import MaskedInput from "react-text-mask";
+import {FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText} from "reactstrap";
 
 const renderFormInput = (name, label, handleInputChange, options = {}) => {
-    let classInput = options.size == 'full'? 'w-full' : "w-1/2 md:w-full";
-    const margins = 'px-3 pb-3';
-    const defaultClasses = "bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500"
-    const id = `form-input${name}`;
-    let input = (
-        <input type={options.type || 'text'} id={id} name={name}
-            className={`${defaultClasses} ${margins}`}
-            pattern={options.pattern || null}
-            onChange={handleInputChange} /
-        >);
-    if (options.mask) {
-        input = (
-            <MaskedInput type={options.type || 'text'} id={id} name={name}
-                className={`${defaultClasses} ${margins}`}
-                mask={options.mask}
-                guide={true}
-                onChange={handleInputChange} /
-            >);
-    }
-    return (
-        <div className={`${classInput} px-4`}>
-            <div className="mb-6 pt-3 rounded bg-gray-200">
-                <label htmlFor={id} className="block text-gray-700 text-sm font-bold mb-2 ml-3">{label}</label>
-                {input}
-            </div>
-        </div>
+  const {additionalClass} = options;
+  const id = `form-input${name}`;
+  let input = (
+    <input type={options.type || 'text'} id={id} name={name}
+           pattern={options.pattern || null}
+           onChange={handleInputChange}
+           placeholder={label}
+           className="form-control"
+    />
+  );
+  if (options.mask) {
+    input = (
+      <MaskedInput type={options.type || 'text'} id={id} name={name}
+                   mask={options.mask}
+                   guide={true}
+                   placeholder={label}
+                   onChange={handleInputChange}
+                   className="form-control"
+      />
     );
+  }
+  return (
+    <FormGroup className="col-md-6">
+      <InputGroup className="input-group-alternative mb-3 input-group">
+        <InputGroupAddon addonType="prepend">
+          <InputGroupText>
+            <i className="ni ni-hat-3"/>
+          </InputGroupText>
+        </InputGroupAddon>
+        {input}
+      </InputGroup>
+    </FormGroup>
+  );
 }
 
 export {renderFormInput};
